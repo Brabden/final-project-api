ad0f99f829115664ea3669385cbf671becd238b6
Brab <brandon.tossan@gmail.com>
2026-01-05 11:34:49 -0500

    added / to update-profile for consistency

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path("login/", views.login_user, name="login"),
     path("signup/", views.signup, name="signup"),
     path("profile/", views.profile, name="profile"),
-    path("update-profile", views.update_profile, name="update_profile"),
+    path("update-profile/", views.update_profile, name="update_profile"),
     path("change-password/", views.change_password, name="change_password"),
     path("support-request/", views.support_request, name="support_request"),
 ]
