4d05c31f26d4b178129e15b0ed6ccdd2dd66f693
Brab <brandon.tossan@gmail.com>
2026-01-17 18:21:11 -0500

    added individual product page

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import KeyboardListCreateView
+from .views import KeyboardListCreateView, KeyboardDetailView
 
 urlpatterns = [
     path('keyboards/', KeyboardListCreateView.as_view(), name="keyboard-list-create"),
+    path('keyboards/<int:pk>/', KeyboardDetailView.as_view(), name="keyboard-detail"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 from .models import Keyboard
 from .serializers import KeyboardSerializer
 
@@ -18,3 +19,8 @@ class KeyboardListCreateView(APIView):
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class KeyboardDetailView(APIView):
+    def get(self, request, pk):
+        keyboard = get_object_or_404(Keyboard, pk=pk)
+        serializer = KeyboardSerializer(keyboard)
+        return Response(serializer.data)
